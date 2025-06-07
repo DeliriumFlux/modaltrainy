ea5803dd9ac2f4fe60057ec99abf092de7b39679
DeliriumFlux <rocksonix44@gmail.com>
2025-06-07 17:42:13 +0300

    pre final of test

--- js/script.dev.js
+++ js/script.dev.js
@@ -32,6 +32,74 @@ document.addEventListener('DOMContentLoaded', function () {
     if (ev.code === 'Escape' && modal.classList.contains('show')) {
       openCloseModal();
     }
-  });
+  }); // email promo validation
+
+  var emailInput = document.querySelector('#email'),
+      promoBtn = document.querySelector('.btn-promo');
+  promoBtn.addEventListener('click', function () {
+    if (emailInput.value === '') {
+      alert('Введите ваш E-mail');
+    } else {
+      alert('Промокод добавлен');
+    }
+  }); // order functionality
+
+  var btnAddToCart = document.querySelector('.btn__round-plus'),
+      btnRemoveFromCart = document.querySelector('.btn__round-minus'),
+      btnSubmit = document.querySelector('.btn-submit'),
+      productNumbers = document.querySelector('.modal__product-numbers'),
+      numberGoods = document.querySelector('.modal__numbergoods'),
+      priceForm = document.querySelector('.modal__price'),
+      totalPrice = document.querySelector('.modal__totalprice'),
+      checkbox = document.querySelector('#checkoffert'),
+      basePrice = 1790;
+  var count = 1;
+
+  function updatePrice() {
+    priceForm.textContent = basePrice;
+    return "".concat(basePrice * count, "\u20BD");
+  }
+
+  function countsAndNumberOfGoods() {
+    productNumbers.textContent = count;
+    numberGoods.textContent = "\u0422\u043E\u0432\u0430\u0440\u044B (".concat(count, ")");
+  }
+
+  function addAndRemoveCard() {
+    if (count > 0) {
+      btnAddToCart.addEventListener('click', function () {
+        count++;
+        countsAndNumberOfGoods();
+        totalPrice.textContent = updatePrice();
+      });
+      btnRemoveFromCart.addEventListener('click', function () {
+        count--;
+        countsAndNumberOfGoods();
+
+        if (count <= 0) {
+          count = 1;
+          countsAndNumberOfGoods();
+        }
+
+        totalPrice.textContent = updatePrice();
+      });
+    }
+  }
+
+  function submitProduct() {
+    btnSubmit.addEventListener('click', function (ev) {
+      ev.preventDefault();
+
+      if (checkbox.checked) {
+        window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+      } else {
+        alert('Согласитесь с условиями');
+      }
+    });
+  }
+
+  updatePrice();
+  addAndRemoveCard();
+  submitProduct();
 });
 //# sourceMappingURL=script.dev.js.map
